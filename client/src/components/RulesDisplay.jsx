79e85a7b5e496ec5979735d0e60281341ec5c832
OLIVIERO MARCO s361264 <s361264@studenti.polito.it>
2026-06-20 19:08:20 +0200

    feat: wrote the rules

--- client/src/components/RulesDisplay.jsx
+++ client/src/components/RulesDisplay.jsx
@@ -5,10 +5,27 @@ function RulesDisplay(props){
         <Container>
             <h2>Rules</h2>
             <ol>
-                <li><strong>Setup:</strong></li>
-                <li><strong>Plan:</strong></li>
-                <li><strong>Execution:</strong></li>
-                <li><strong>Result:</strong></li>
+                <li><strong>Setup:</strong> In this phase, the player will
+                see the network map, with the lines connecting the stations.
+                His aim is to memorize it for the next phase. Whenever he is ready,
+                he can click on th "GO ON" button to go to the next phase.</li>
+                <li><strong>Plan:</strong> In this phase, the player will see the network map
+                with only the stations on the left, on the right the list of the connections.
+                In the map, two staions are highlighted: the starting and the finish stations.
+                The player has 90 seconds to create a route, that is a sequence of connections,
+                to connect the start and finish station. Once done, he has to click the "SUBMIT" button
+                to save it. If the timer expires, the route will be automatically submitted.
+                To add a connection to the route, he has to click it on the list, if he
+                wants to remove it, he has to click it again.
+                Be aware that a route is valid if: it starts from the "start" station and finishes
+                at the "finish" one; there is no extra connections apart the ones building the route.</li>
+                <li><strong>Execution:</strong> The game will show the submitted and valid route.
+                For each connection, it will show a random event. This event will increment/decrement/not affect
+                the player's coins, which start from 20.</li>
+                <li><strong>Result:</strong>Here the result is shown on the left. If the route was invalid, the score is
+                automatically set to 0; same if the coins went negative. On the right the ranking board
+                is shown. Then if the player wants to play another game, he has to click on the "New Game" button,
+                if not, he can click the "Home" button to go bakc to the home.</li>
             </ol>
         </Container>
     )
