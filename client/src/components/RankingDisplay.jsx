d1fc0f08137ed872f326628d44f58ab8925c298a
OLIVIERO MARCO s361264 <s361264@studenti.polito.it>
2026-06-22 19:41:34 +0200

    fix: added the position in the ranking

--- client/src/components/RankingDisplay.jsx
+++ client/src/components/RankingDisplay.jsx
@@ -1,30 +1,28 @@
 import { Container, Table, Row, Col, Button } from "react-bootstrap";
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-
-import { getPlayers } from "../api/api";
 
 function RankingDisplay(props){
     const players = props.players;
 
     const navigate = useNavigate();
 
-    const sortedPlayers = [...players].sort((a, b) => b.best_score-a.best_score);
+    const sortedPlayers = [...players].filter((p) => p.best_score !== 0).sort((a, b) => b.best_score-a.best_score);
 
     return( <Container className="mt-1">
             <Row className="justify-content-center">
                     <Table striped bordered hover>
                         <thead>
                             <tr>
-                                <th colSpan={2}>RANKING BOARD</th>
+                                <th colSpan={3}>RANKING BOARD</th>
                             </tr>
                             <tr>
+                                <th>Position</th>
                                 <th>Username</th>
                                 <th>Best score</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {sortedPlayers.map((p) => <PlayerRow key={p.username} p={p}></PlayerRow>)}
+                            {sortedPlayers.map((p, index) => <PlayerRow key={p.username} p={p} position={index+1}></PlayerRow>)}
                         </tbody>
                     </Table>
             </Row>
@@ -41,9 +39,11 @@ function RankingDisplay(props){
 
 function PlayerRow(props){
     const p = props.p;
+    const position = props.position;
 
     return (
         <tr>
+            <td>{position}</td>
             <td>{p.username}</td>
             <td>{p.best_score}</td>
         </tr>
